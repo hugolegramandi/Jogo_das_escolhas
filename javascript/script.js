696bbef52a71498d80ed18a4589faaf9f34b3ca8
Hugo Legramandi <hugolegramandi@gmail.com>
2022-01-10 11:07:02 -0300

    Commit Estrutura de páginas
    Estruturação de páginas e links entre elas através de prompt com Javascript

---
+++ javascript/script.js
@@ -0,0 +1,167 @@
+  // SCRIPTS CIENTISTA
+
+  function cientista_1() {
+    while (question != 1 || question != 2) { //Aqui são os numeros que devem ser digitados como resposta no prompt
+      var question = prompt(
+        '1. \n 2.' //Aqui escrever as duas opçoes do personagem
+      );
+  
+      if (question == 1) {
+        window.location.href = "../stories/cientista_2.html"; // Se DIGITAR 1, vai para página 2 do personagem
+        break;
+      } else if (question == 2) {
+        window.location.href = "../stories/cientista_gameover.html"; // Se DIGITAR 2, vai para página game over do personagem
+        break;
+      } else {
+        alert("Digite 1 ou 2");
+      }
+    }
+  }
+
+  function cientista_2() {
+    while (question != 1 || question != 2) { //Aqui são os numeros que devem ser digitados como resposta no prompt
+      var question = prompt(
+        '1. \n 2.' //Aqui escrever as duas opçoes do personagem
+      );
+  
+      if (question == 1) {
+        window.location.href = "../stories/cientista_3.html"; // Se DIGITAR 1, vai para página 2 do personagem
+        break;
+      } else if (question == 2) {
+        window.location.href = "../stories/cientista_gameover.html"; // Se DIGITAR 2, vai para página game over do personagem
+        break;
+      } else {
+        alert("Digite 1 ou 2");
+      }
+    }
+  }
+
+  function cientista_3() {
+    while (question != 1 || question != 2) { //Aqui são os numeros que devem ser digitados como resposta no prompt
+      var question = prompt(
+        '1. \n 2.' //Aqui escrever as duas opçoes do personagem
+      );
+  
+      if (question == 1) {
+        window.location.href = "../stories/cientista_win.html"; // Se DIGITAR 1, vai para página 2 do personagem
+        break;
+      } else if (question == 2) {
+        window.location.href = "../stories/cientista_gameover.html"; // Se DIGITAR 2, vai para página game over do personagem
+        break;
+      } else {
+        alert("Digite 1 ou 2");
+      }
+    }
+  }
+
+  // SCRIPTS MÉDICO
+
+  function medico_1() {
+    while (question != 1 || question != 2) { //Aqui são os numeros que devem ser digitados como resposta no prompt
+      var question = prompt(
+        '1. \n 2.' //Aqui escrever as duas opçoes do personagem
+      );
+  
+      if (question == 1) {
+        window.location.href = "../stories/medico_2.html"; // Se DIGITAR 1, vai para página 2 do personagem
+        break;
+      } else if (question == 2) {
+        window.location.href = "../stories/medico_gameover.html"; // Se DIGITAR 2, vai para página game over do personagem
+        break;
+      } else {
+        alert("Digite 1 ou 2");
+      }
+    }
+  }
+
+  function medico_2() {
+    while (question != 1 || question != 2) { //Aqui são os numeros que devem ser digitados como resposta no prompt
+      var question = prompt(
+        '1. \n 2.' //Aqui escrever as duas opçoes do personagem
+      );
+  
+      if (question == 1) {
+        window.location.href = "../stories/medico_3.html"; // Se DIGITAR 1, vai para página 2 do personagem
+        break;
+      } else if (question == 2) {
+        window.location.href = "../stories/medico_gameover.html"; // Se DIGITAR 2, vai para página game over do personagem
+        break;
+      } else {
+        alert("Digite 1 ou 2");
+      }
+    }
+  }
+
+  function medico_3() {
+    while (question != 1 || question != 2) { //Aqui são os numeros que devem ser digitados como resposta no prompt
+      var question = prompt(
+        '1. \n 2.' //Aqui escrever as duas opçoes do personagem
+      );
+  
+      if (question == 1) {
+        window.location.href = "../stories/medico_win.html"; // Se DIGITAR 1, vai para página 2 do personagem
+        break;
+      } else if (question == 2) {
+        window.location.href = "../stories/medico_gameover.html"; // Se DIGITAR 2, vai para página game over do personagem
+        break;
+      } else {
+        alert("Digite 1 ou 2");
+      }
+    }
+  }
+
+// SCRIPTS PROFESSOR
+
+function professor_1() {
+    while (question != 1 || question != 2) { //Aqui são os numeros que devem ser digitados como resposta no prompt
+      var question = prompt(
+        '1. \n 2.' //Aqui escrever as duas opçoes do personagem
+      );
+  
+      if (question == 1) {
+        window.location.href = "../stories/professor_2.html"; // Se DIGITAR 1, vai para página 2 do personagem
+        break;
+      } else if (question == 2) {
+        window.location.href = "../stories/professor_gameover.html"; // Se DIGITAR 2, vai para página game over do personagem
+        break;
+      } else {
+        alert("Digite 1 ou 2");
+      }
+    }
+  }
+
+  function professor_2() {
+    while (question != 1 || question != 2) { //Aqui são os numeros que devem ser digitados como resposta no prompt
+      var question = prompt(
+        '1. \n 2.' //Aqui escrever as duas opçoes do personagem
+      );
+  
+      if (question == 1) {
+        window.location.href = "../stories/professor_3.html"; // Se DIGITAR 1, vai para página 2 do personagem
+        break;
+      } else if (question == 2) {
+        window.location.href = "../stories/professor_gameover.html"; // Se DIGITAR 2, vai para página game over do personagem
+        break;
+      } else {
+        alert("Digite 1 ou 2");
+      }
+    }
+  }
+
+  function professor_3() {
+    while (question != 1 || question != 2) { //Aqui são os numeros que devem ser digitados como resposta no prompt
+      var question = prompt(
+        '1. \n 2.' //Aqui escrever as duas opçoes do personagem
+      );
+  
+      if (question == 1) {
+        window.location.href = "../stories/professor_win.html"; // Se DIGITAR 1, vai para página 2 do personagem
+        break;
+      } else if (question == 2) {
+        window.location.href = "../stories/professor_gameover.html"; // Se DIGITAR 2, vai para página game over do personagem
+        break;
+      } else {
+        alert("Digite 1 ou 2");
+      }
+    }
+  }
